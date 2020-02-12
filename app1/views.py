f14654a164faf351687bc7ef63a73d58c3331dd5
ryo.kaneoka0406@gmail.com <ryo.kaneoka0406@gmail.com>
2020-02-13 08:43:30 +0900

    developping view

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import DocumentForm
 from .models import Document
+import cv2
+from imageapp.settings import BASE_DIR
 
 
 def index(request):
@@ -14,8 +16,24 @@ def index(request):
         form = DocumentForm()
         max_id = Document.objects.latest('id').id
         obj = Document.objects.get(id=max_id)
+        # 画像のURLを取得したい_けどopenCVで画像が読み取れない。
+        # パーミッションエラーかしら
+        input_path = BASE_DIR + obj.photo.url
+        output_path = BASE_DIR + "/media/processed/processed.jpg"
+        print(input_path)
+        # PATHは正しい
+        process(input_path, output_path)
+        obj.process = "/media/processed/processed.jpg"
+        obj.save()
 
     return render(request, 'app1/index.html', {
         'form': form,
         'obj': obj
     })
+
+
+def process(input_path, output_path):
+    img = cv2.imread(input_path)
+    # エラー発生
+    img_processed = cv2.cvtColor(img, cv2.COLOR_BAYER_BG2GRAY)
+    cv2.imwrite(output_path, img_processed)
