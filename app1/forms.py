538045fae2b4ffe825bc97cba6086dae2d7c7d34
ryo.kaneoka0406@gmail.com <ryo.kaneoka0406@gmail.com>
2020-02-16 22:07:19 +0900

    fix process

--- app1/forms.py
+++ app1/forms.py
@@ -5,4 +5,4 @@ from .models import Document
 class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
-        fields = ('description', 'photo', )
+        fields = ('photo', )
--- app1/views.py
+++ app1/views.py
@@ -11,33 +11,37 @@ def index(request):
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            return redirect('process')
     else:
         # form描画
         form = DocumentForm()
 
-        # 最新のデータ取得
-        max_id = Document.objects.latest('id').id
-        obj = Document.objects.get(id=max_id)
-
-        # pathの取得
-        input_path = BASE_DIR + obj.photo.url
-        before_filename = os.path.splitext(os.path.basename(input_path))[0]
-        after_filename = before_filename + "_processed.jpg"
-        output_path = BASE_DIR + "/media/processed/" + after_filename
-
-        # 処理＆Record
-        process(input_path, output_path)
-        obj.processed = "/processed/" + after_filename
-        obj.save()
-
-    return render(request, 'app1/index.html', {
-        'form': form,
-        'obj': obj
-    })
+    return render(request, 'app1/index.html', {'form': form, })
 
 
-def process(input_path, output_path):
+def process(request):
+    max_id = Document.objects.latest('id').id
+    obj = Document.objects.get(id=max_id)
+
+    if request.method == 'POST':
+        if 'button_process' in request.POST:
+            # pathの取得
+            input_path = BASE_DIR + obj.photo.url
+            before_filename = os.path.splitext(os.path.basename(input_path))[0]
+            after_filename = before_filename + "_processed.jpg"
+            output_path = BASE_DIR + "/media/processed/" + after_filename
+
+            # 処理＆Record
+            gray(input_path, output_path)
+            obj.processed = "/processed/" + after_filename
+            obj.save()
+
+            return render(request, 'app1/result.html', {'obj': obj})
+
+    return render(request, 'app1/process.html', {'obj': obj})
+
+
+def gray(input_path, output_path):
     img = cv2.imread(input_path)
     im_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     cv2.imwrite(output_path, im_gray, [cv2.IMWRITE_JPEG_QUALITY, 100])
