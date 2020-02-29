3c470c8c7b8c63a33aa25605120debb6578c8659
ryo.kaneoka0406@gmail.com <ryo.kaneoka0406@gmail.com>
2020-02-29 19:07:11 +0900

    start to test

--- app1/tests.py
+++ app1/tests.py
@@ -6,3 +6,22 @@ class TestIndex(TestCase):
     def test_get(self):
         res = self.client.get(reverse('index'))
         self.assertTemplateUsed(res, 'app1/index.html')
+
+    def test_post(self):
+        pass
+
+
+class TestProcess(TestCase):
+    def test_get(self):
+        pass
+
+    def test_post(self):
+        pass
+
+
+class TestGray(TestCase):
+    def test_grayed(self):
+        pass
+
+    def test_error(self):
+        pass
