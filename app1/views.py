07d1aad4bb77b9a6449d3e12a26bc8cf0b61551b
ryo.kaneoka0406@gmail.com <ryo.kaneoka0406@gmail.com>
2020-03-26 09:53:37 +0900

    test

--- app1/views.py
+++ app1/views.py
@@ -96,7 +96,7 @@ def gray(input_url):
         input_filename = os.path.basename(input_url_with_noQ)
         output_filename = os.path.splitext(input_filename)[
             0] + '_processed.jpg'
-        output_pass = 'processed/' + output_filename
+        output_pass = BASE_DIR + "/media/processed/" + output_filename
 
         with tempfile.TemporaryDirectory() as tmpdir:
             # GCSの処理対象画像を一時ディレクトリにダウンロード
@@ -107,10 +107,10 @@ def gray(input_url):
             blob.download_to_filename(downloadDir)
 
             # 画像を置いた一時ディレクトリから読み取り、処理へ
-            img = cv2.imread(downloadDir)
-            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            cv2.imwrite(os.path.join(tmpdir, output_filename),
-                        img_gray, [cv2.IMWRITE_JPEG_QUALITY, 100])
+            # img = cv2.imread(downloadDir)
+            # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # cv2.imwrite(os.path.join(tmpdir, output_filename),
+            #             img_gray, [cv2.IMWRITE_JPEG_QUALITY, 100])
 
             # 画像のGCSへのアップロード
             client = storage.Client()
